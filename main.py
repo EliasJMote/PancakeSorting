de93e10ad80f61f1387f8e97d34d470a2e5efa14
Elias James Mote <eliasjmote@gmail.com>
2018-09-26 13:24:58 -0700

    Completed project 2 with BFS, added main.py as driver function

---
+++ main.py
@@ -0,0 +1,13 @@
+# Project 2
+# Created by: Elias Mote and Justin Ramos
+# Date: 9/26/18
+
+import project_2
+
+# Get permutation input from the user
+def main():
+	project_2.get_input()
+
+# Run the main function
+if __name__ == "__main__":
+	main()
